30ee1f5e2dfaa01c65d6ba625f703da5bb415f6d
Jean-Marc (PC) <mjean_marc@hotmail.com>
2021-10-18 09:56:01 +0200

    Add parameters

---
+++ Create_Csv_File.php
@@ -0,0 +1,16 @@
+<?php
+
+class Create_Csv_File {
+
+    private $data = [];
+
+    private $filepath = '';
+
+    private $first_line = '';
+
+    function __construct($data, $filepath, $first_line = '') {
+
+
+    }
+    
+}
